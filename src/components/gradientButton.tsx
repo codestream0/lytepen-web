4cdc124fe90c6da5c1014b65c0ef0d58db13218c
codestream0 <hamzaabdulfatah235@gmail.com>
2025-11-15 21:54:58 +0100

    added functionality

--- src/components/gradientButton.tsx
+++ src/components/gradientButton.tsx
@@ -8,20 +8,34 @@ interface GradientButtonProps {
   onClick?: () => void
 }
 
-const GradientButton: React.FC<GradientButtonProps> = ({ text = "Get Started Now", onClick }) => {
+const GradientButton: React.FC<GradientButtonProps> = ({
+  text = "Get Started Now",
+  onClick,
+}) => {
   return (
-    <div onClick={onClick} className="flex gap-1 pl-1.5  bg-white rounded-full items-center hover:scale-101 transition-all duration-200 ">
-     <span className="flex items-center justify-center w-6 h-6 rounded-full bg-blue-900 text-white">
-        <ArrowRight size={16} />
-    </span>
     <button
-      className=" flex items-center rounded-full bg-linear-to-r from-yellow-400 to-orange-500 px-5 py-3  font-poppins text-[10px] text-[#151A35] shadow-md hover:shadow-lg "
+      onClick={onClick}
+      className="
+        group flex items-center gap-2 h-9 min-w-44 rounded-full 
+        bg-linear-to-r from-yellow-400 to-orange-500
+        font-poppins text-[10px] text-[#151A35]
+        shadow-md hover:shadow-lg transition-all duration-200
+      "
     >
+      <span
+        className="
+          flex items-center justify-center h-9 px-3 w-14 rounded-full bg-white
+          opacity-0 scale-75 -translate-x-2
+          group-hover:opacity-100 group-hover:scale-100 group-hover:translate-x-0
+          transition-all duration-300
+        "
+      >
+        <ArrowRight size={20} className="text-blue-900 font-extrabold " />
+      </span>
 
-      {text}
+      <span className="pr-5 py-3" >{text}</span>
     </button>
-    </div>
   )
 }
 
-export default GradientButton;
+export default GradientButton
